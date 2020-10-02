21da35ba319e4cf65678e5754487f70b0beec1a1
Matthew Day <matt@matthewday.net>
2020-10-01 22:00:53 -0400

    Change server port to 8080

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(8000, () => {
-  console.log("Express server is listening on port 8000!");
+app.listen(8080, () => {
+  console.log("Express server is listening on port 8080!");
 });
